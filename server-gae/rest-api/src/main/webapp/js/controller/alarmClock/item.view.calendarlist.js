8436f9d1decf06fceee019acbed65b076770dc95
maximepvrt <maximepvrt@gmail.com>
2014-11-18 09:42:59 +0100

    fix bug js

--- server-gae/rest-api/src/main/webapp/js/controller/alarmClock/item.view.calendarlist.js
+++ server-gae/rest-api/src/main/webapp/js/controller/alarmClock/item.view.calendarlist.js
@@ -3,7 +3,7 @@ var controller = angular.module('myLazyClock.controller.alarmClock.item.view.cal
 controller.controller('myLazyClock.controller.alarmClock.item.view.calendarlist', ['$scope', 'GApi', '$stateParams',
     function homeCtl($scope, GApi, $stateParams) {
     	$scope.calendars = {}
-    	GApi.executeAuth('myLazyClock', 'calendar.list').then( function(resp) {
+    	GApi.executeAuth('myLazyClock', 'calendar.list', {'alarmClockId': $stateParams.id}).then( function(resp) {
     		if (resp.items != undefined)
                	$scope.calendars = resp.items;
         });
--- server-gae/rest-api/src/main/webapp/js/controller/page.js
+++ server-gae/rest-api/src/main/webapp/js/controller/page.js
@@ -2,7 +2,7 @@ var controller = angular.module('myLazyClock.controller.page', []);
 
 controller.controller('myLazyClock.controller.page', ['$rootScope', 'GApi',
     function homeCtl($rootScope, GApi) {
-    	$rootScope.alarmClocks = {}
+    	$rootScope.alarmClocks = [];
         GApi.executeAuth('myLazyClock', 'alarmClock.list').then(function(resp) {
         		if (resp.items != undefined)
                 	$rootScope.alarmClocks = resp.items;
