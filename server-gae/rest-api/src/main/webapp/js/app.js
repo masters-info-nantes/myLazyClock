146161d3bce879e1d360ab905215aa3cb06285a5
maximepvrt <maximepvrt@gmail.com>
2014-12-09 18:19:17 +0100

    add preparation_times

--- server-gae/rest-api/src/main/webapp/js/app.js
+++ server-gae/rest-api/src/main/webapp/js/app.js
@@ -14,13 +14,34 @@ app.constant('TRAVELS_MODE',[
     {'name' : 'A pied', 'id' : 'WALKING'},
     {'name' : 'En voiture', 'id' : 'DRIVING'},
     {'name' : 'En vélo', 'id' : 'BICYCLING'},
-    {'name' : 'En Transport', 'id' : 'TRANSIT'}
+    {'name' : 'En Transport en commun', 'id' : 'TRANSIT'}
 ]);
 
 app.constant('PREPARATION_TIMES',[
     {'name' : '5 minutes', 'time' : '300'},
+    {'name' : '10 minutes', 'time' : '600'},
     {'name' : '15 minutes', 'time' : '900'},
-    {'name' : '20 minutes', 'time' : '1200'}
+    {'name' : '20 minutes', 'time' : '1200'},
+    {'name' : '25 minutes', 'time' : '1500'},
+    {'name' : '30 minutes', 'time' : '1800'},
+    {'name' : '35 minutes', 'time' : '2100'},
+    {'name' : '40 minutes', 'time' : '2400'},
+    {'name' : '45 minutes', 'time' : '2700'},
+    {'name' : '50 minutes', 'time' : '3000'},
+    {'name' : '55 minutes', 'time' : '3300'},
+    {'name' : '1 heure', 'time' : '3600'},
+    {'name' : '1 heure et 5 minutes', 'time' : '3900'},
+    {'name' : '1 heure et 10 minutes', 'time' : '4200'},
+    {'name' : '1 heure et 15 minutes', 'time' : '4500'},
+    {'name' : '1 heure et 20 minutes', 'time' : '4800'},
+    {'name' : '1 heure et 25 minutes', 'time' : '5100'},
+    {'name' : '1 heure et 30 minutes', 'time' : '5400'},
+    {'name' : '1 heure et 35 minutes', 'time' : '5470'},
+    {'name' : '1 heure et 40 minutes', 'time' : '6000'},
+    {'name' : '1 heure et 45 minutes', 'time' : '6300'},
+    {'name' : '1 heure et 50 minutes', 'time' : '6600'},
+    {'name' : '1 heure et 55 minutes', 'time' : '6900'},
+    {'name' : '2 heures', 'time' : '7200'}
 ]);
 
 app.config(function ($provide) {
