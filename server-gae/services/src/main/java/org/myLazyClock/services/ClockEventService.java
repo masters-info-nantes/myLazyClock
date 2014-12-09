771f4b2e14431dfe8a5986fa11b9b5cb9b71afe8
Dralagen <dralagen@dralagen.fr>
2014-12-09 15:45:54 +0100

    delete useless service and review ClockEventService

--- server-gae/services/src/main/java/org/myLazyClock/services/ClockEventService.java
+++ server-gae/services/src/main/java/org/myLazyClock/services/ClockEventService.java
@@ -1,18 +1,25 @@
 package org.myLazyClock.services;
 
 import org.myLazyClock.calendarApi.CalendarEvent;
+import org.myLazyClock.calendarApi.CalendarFactory;
+import org.myLazyClock.calendarApi.CalendarStrategy;
 import org.myLazyClock.calendarApi.EventNotFoundException;
 import org.myLazyClock.model.model.AlarmClock;
 import org.myLazyClock.model.model.Calendar;
 import org.myLazyClock.model.model.MyLazyClockUser;
 import org.myLazyClock.model.repository.AlarmClockRepository;
-import org.myLazyClock.model.repository.CalendarRepository;
 import org.myLazyClock.services.bean.AlarmClockEvent;
 import org.myLazyClock.travelApi.TravelDuration;
 import org.myLazyClock.travelApi.TravelFactory;
 import org.myLazyClock.travelApi.TravelStrategy;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
 
 /**
  * Created on 30/11/14.
@@ -37,7 +44,7 @@ public class ClockEventService {
 
         String userToken = (user != null) ? user.getToken() : "";
 
-        Collection<Calendar> calendarList = CalendarRepository.getInstance().findAll(alarmClock);
+        Collection<Calendar> calendarList = alarmClock.getCalendars();
 
         TimeZone.setDefault(TimeZone.getTimeZone("Europe/Paris")); // For new Date()
         java.util.Calendar currentCal = java.util.Calendar.getInstance();
@@ -47,20 +54,17 @@ public class ClockEventService {
         currentCal.set(java.util.Calendar.HOUR_OF_DAY, 23);
         currentCal.set(java.util.Calendar.MINUTE, 59);
 
-        CalendarModulesService serviceCalendar = CalendarModulesService.getInstance();
-        Collection<AlarmClockEvent> eventsInWeek = new ArrayList<AlarmClockEvent>();
+        Collection<AlarmClockEvent> eventsInWeek = new ArrayList<>();
 
         // Search first event for each day of week
         for(int i = 0; i < 7; i++){
 
-            ArrayList<AlarmClockEvent> eventsInDay = new ArrayList<AlarmClockEvent>();
+            ArrayList<AlarmClockEvent> eventsInDay = new ArrayList<>();
             for(Calendar cal: calendarList){
                 try {
 
-                    CalendarEvent eventOfDay = serviceCalendar.getFirstEventOfDay(cal, currentCal, userToken);
-                    if (cal.isUseAlwaysDefaultLocation()) {
-                        eventOfDay.setAddress(cal.getDefaultEventLocation());
-                    } else if (eventOfDay.getAddress() == null || eventOfDay.equals("")) {
+                    CalendarEvent eventOfDay = getFirstEventOfDay(cal, currentCal, userToken);
+                    if (cal.isUseAlwaysDefaultLocation() || eventOfDay.getAddress() == null || eventOfDay.getAddress().equals("")) {
                         eventOfDay.setAddress(cal.getDefaultEventLocation());
                     }
                     AlarmClockEvent alarmClockEventOfDay = calendarEventToAlarmClockEvent(eventOfDay);
@@ -74,7 +78,7 @@ public class ClockEventService {
             }
 
             // Add sooner event of day in list
-            AlarmClockEvent alarmEvent = null;
+            AlarmClockEvent alarmEvent;
 
             if(!eventsInDay.isEmpty()){
                 alarmEvent = Collections.min(eventsInDay);
@@ -86,10 +90,51 @@ public class ClockEventService {
             }
 
             // Next day
-            currentCal.setTimeInMillis(currentCal.getTimeInMillis() + (24 * 60 * 60 * 1000));
+            currentCal.add(java.util.Calendar.DATE, 1);
         }
 
         return eventsInWeek;
+    }
+
+    /**
+     * Return the first event in specified day on online schedule
+     *
+     * @param calendar Calendar to use with settings (strategy, url)
+     * @param date Day where to search event
+     * @return First event of day if exist, null otherwise
+     */
+
+    public CalendarEvent getFirstEventOfDay(Calendar calendar, java.util.Calendar date, String token) throws EventNotFoundException{
+
+
+        int strategyId; // ICS_FILE
+
+        String calendarType = calendar.getCalendarType();
+
+        Map<String, String> params = new HashMap<>();
+
+        switch (calendarType) {
+            case "EDT":
+                strategyId = 2;
+                params.put("groupId", calendar.getParam());
+                break;
+            case "GOOGLE_CALENDAR":
+                strategyId = 3;
+                params.put("gCalId", calendar.getParam());
+                params.put("tokenRequest", token);
+
+                params.put("apiId", ConstantAPI.API_ID);
+                params.put("apiSecret", ConstantAPI.API_SECRET);
+                break;
+            default:
+                strategyId = 1; // URL of ICS file
+                params.put("url", calendar.getParam());
+                break;
+        }
+
+        CalendarStrategy strategy = CalendarFactory.getInstance().get(strategyId);
+
+        return strategy.getFirstEvent(calendar.getParam(), date, params);
     }
 
     private AlarmClockEvent calendarEventToAlarmClockEvent(CalendarEvent calendarEvent) {
@@ -107,7 +152,7 @@ public class ClockEventService {
     private Long getDuration (AlarmClock alarmClock, AlarmClockEvent event) {
         TravelStrategy strategy;
 
-        Map<String, String> params = new HashMap<String, String>();
+        Map<String, String> params = new HashMap<>();
 
         switch (event.getTravelMode()) {
             case BICYCLING:
