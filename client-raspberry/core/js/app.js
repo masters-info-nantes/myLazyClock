3bbe7ace10d68b56f0ed68bd0e9d5915a86c1fb7
maximepvrt <maximepvrt@gmail.com>
2014-12-12 23:30:37 +0100

    fix base api raspclient

--- client-raspberry/core/js/app.js
+++ client-raspberry/core/js/app.js
@@ -14,16 +14,7 @@ var app = angular.module('myLazyClock', [
 app.run(['GApi', '$state',
     function(GApi, $state) {
 
-        var BASE = 'https://mylazyclock.appspot.com/_ah/api';
-        if(window.location.hostname == 'localhost') {
-            if(window.location.port == '8080') {
-                BASE = 'http://localhost:8080/_ah/api';
-            } else {
-                BASE = 'http://localhost:8080/_ah/api';
-            } 
-        } else {
-            BASE = 'https://mylazyclock.appspot.com/_ah/api';
-        }
+        //var BASE = 'http://localhost:8080/_ah/api';
         var BASE = 'https://mylazyclock.appspot.com/_ah/api';
 
         GApi.load('myLazyClock','v1',BASE);
